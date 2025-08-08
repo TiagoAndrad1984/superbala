64743e0bdc788121ff12536f368f1a3c95bfc0bf
TiagoAndrad1984 <37714901+TiagoAndrad1984@users.noreply.github.com>
2025-08-08 12:19:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<html>
+
+  <head>
+     <title></title>
+	 <link rel="stylesheet" href="estilo_um.css" />
+	 <meta name="viewport" content="width = device-width, initial-scale=1.0">
+  </head>
+
+  <body>
+     <main class="container">
+	     <form>
+		    <div class="input-x">
+			  <h1>Login</h1>
+			   <input type="email" name="log" placeholder="username"/>
+			</div> 
+            <div class="input-x">			
+			    <h1>senha</h1>
+			   <input type="password" name="senha" placeholder="senha"/>
+			</div>
+			<div class="renemb">
+			   <label>
+			   Lembrar minha senha:
+			   <input type="checkbox"/>
+			   
+			   </label>
+			   </br>
+			   <a href="#" class="sen">Esqueci minha senha</a>
+			</div>
+		 <button type="submit">Login</button>	
+		 <div class="registr">
+		   <a class="cad">Não tem uma conta? <a href="pgcad.php" class="cad"> Cadastre-se</a></a>
+		 </div>
+		 </form>
+	 </main>
+    
+  </body>
+
+
+</html>
+
+<?php 
+
+
+
+?>
